2c7241dddc0ae96f25a63329fc70fc4207359a2c
Adam Turowski <horosporo@gmail.com>
2021-08-29 01:34:23 +0200

    Fixed verifyToken function (token must exist in redis cache)

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -1,16 +1,26 @@
 const jwt = require("jsonwebtoken");
+const redis = require("redis");
+const { promisify } = require("util");
 
-const tokenError = () => new Error("Token not provided");
+const redisUrl = process.env.REDISCLOUD_URL;
+const redisClient = redis.createClient(redisUrl, { no_ready_check: true });
+redisClient.sismember = promisify(redisClient.sismember);
 
-const verifyToken = (req, res, next) => {
+const tokenError = () => new Error("Token invalid or not provided");
+
+const verifyToken = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return next(tokenError);
+  if (!token) return next(tokenError());
+
+  const tokenValid = await redisClient.sismember("tokens", token);
+  if (!tokenValid) return next(tokenError());
 
   const tokenSecret = process.env.TOKEN_SECRET;
   jwt.verify(token, tokenSecret, (error, user) => {
     if (error) return next(error);
 
+    req.token = token;
     req.user = user;
     next();
   });
